c0cee1d8b6847fd6ef7eba22290d46172bd18deb
Dennis Duka <dukadennis7@gmail.com>
2023-04-01 14:16:02 +0100

    now, we're working with threads!

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,41 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::TcpListener,
+    thread,
+    time::Duration,
 };
+
+use tcprust::ThreadPool;
+
+static _PAGES: &str = "scr/pages";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1703").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-
-        let response = create_response(&stream);
-        stream.write_all(response.as_bytes()).unwrap();
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+}
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let response = create_response(&stream);
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn create_response(mut stream: &std::net::TcpStream) -> String {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "Get / HTTP/1.1" {
-        ("HTTP/1.1 200 OK","hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND","404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "src/pages/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "src/pages/hello.html" )
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "src/pages/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
